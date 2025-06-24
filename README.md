22e757fa1e08a8db9257231d6c7efc14c781cfbb
Jared Jardine <jared@jljsquared.com>
2025-06-24 12:44:01 -0600

    initial Node service

---
+++ README.md
@@ -0,0 +1,13 @@
+# christmas-calculator
+
+This is just a simple Typescript / Node / Express app. Its one endpoint returns the number of days until Christmas or wishes you a 'Merry Christmas'.
+
+It is intended that this project will run with Nodes experimental strip types
+
+Additionally, I have added an "/about" endpoint that returns package name and version as I find this extremely helpful when verifying deployment information. To support this endpoint I have configured Typescript to allow for JSON imports.
+
+## Release Notes (reverse chronological)
+
+### 1.0.0
+
+Initial endpoint and about endpoint.
---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import { daysUntilChristmas } from './utils/days-until-christmas.ts'
+import info from '../package.json' with { type: "json"}
+
+const { name, version } = info
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    const dayCount = daysUntilChristmas(new Date(Date.now()))
+    console.log(`dayCount: ${dayCount}`)
+    if (dayCount === 0) {
+        res.send('Merry Christmas!')
+    } else {
+        res.send(`${dayCount} days until Christmas`)
+    }
+})
+
+app.get('/about', (req, res) => {
+    res.json({
+        name,
+        version
+    })
+})
+
+app.listen(port, () => {
+   return console.log(`Express service listening at htpp://localhost:${port}`) 
+})
